bce5fb14f7d09e50c9495f68b90a96c7e13810e6
Geert Derks <g.a.f.derks@student.tue.nl>
2017-03-13 21:08:33 +0100

    Split up html files further and installed smooth-scrolling.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,15 @@
 window.$ = window.jQuery = require('jquery');
 require('bootstrap-sass');
+
+// fix svg's in IE
 require('svgxuse');
+
+// smooth scrolling for anchor links
+smoothScroll = require('smooth-scroll');
+smoothScroll.init({
+  selector: '[data-scroll]', // Selector for links (must be a class, ID, data attribute, or element tag)
+  selectorHeader: null, // Selector for fixed headers (must be a valid CSS selector) [optional]
+  speed: 500, // Integer. How fast to complete the scroll in milliseconds
+  easing: 'easeInOutCubic', // Easing pattern to use
+  offset: 0 // Integer. How far to offset the scrolling anchor location in pixels
+});
